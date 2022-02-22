bd525335fc0efb23e61991afb8ce4da38ed2f9b7
Jwizzle <info@hossel.net>
2022-02-22 22:28:26 +0100

    First working cobra command.

--- cmd/list.go
+++ cmd/list.go
@@ -7,8 +7,34 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/jwizzle/zettelgo/zettels"
+
 	"github.com/spf13/cobra"
 )
+
+var (
+	display []string
+	display_allow []string = []string{"title", "path"}
+)
+
+// Builds the outputstring from the display var, per note.
+func build_outputstring(note zettels.Note) (string){
+	out := ""
+
+	for _, content := range display {
+		switch content {
+			case "title" :
+				out = out + note.Title + " "
+			case "path" :
+				out = out + note.Path + " "
+			default:
+				//TODO Handle better
+				panic("Malformed display param.")
+		}
+	}
+
+	return out
+}
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
@@ -17,11 +43,16 @@ var listCmd = &cobra.Command{
 	Long: `List all zettels in the directory found in the config file.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		for _, note := range zettelBox.Notes {
-			fmt.Println(note.Title)
+			output := build_outputstring(note)
+			fmt.Println(output)
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+	listCmd.Flags().StringSliceVar(&display, "display", []string{"title"},
+	`Display control. Accepts a comma separated list of:
+	title (default)
+	path`)
 }
