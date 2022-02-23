3559304fdc9269a6a58e0fe0f2ba9112892e8b1a
Jwizzle <info@hossel.net>
2022-02-23 20:15:28 +0100

    Docs.

--- cmd/flaggroups.go
+++ cmd/flaggroups.go
@@ -1,15 +1,16 @@
+// Houses functions to easily add flags to cobra commands.
 package cmd
 
 import (
 	"github.com/spf13/cobra"
 )
 
+// Add to commands with filterable not output.
 var (
 	titleFilter string
 	jsonFilter string
 	tagFilter string
 )
-
 func filterable(cmd *cobra.Command) {
 	listCmd.Flags().StringVar(&titleFilter, "title", "", "Filter results by title.")
 	listCmd.Flags().StringVar(&tagFilter, "tag", "", "Filter results by tag.")
